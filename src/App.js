916426221de3209d087b63b8e926b22faf2a35cc
Admayah <mayobami49@gmail.com>
2024-02-08 15:36:35 +0100

    design header component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+	createBrowserRouter,
+	Route,
+	RouterProvider,
+	Routes,
+} from "react-router-dom";
+
+
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
-
+import Header from "./components/Header";
 import "./App.css";
 
 const router = createBrowserRouter([
 	{
-		path: "/",
-		element: <Home />,
-	},
-	{
-		path: "/sign-up",
-		element: <SignUp />,
-	},
-	{
-		path: "/sign-in",
-		element: <SignIn />,
-	},
-	{
-		path: "/offers",
-		element: <Offers />,
-	},
-	{
-		path: "/profile",
-		element: <Profile />,
-	},
-  {
-		path: "/",
-		element: <Profile />,
-	},
-  {
-		path: "/forgot-password",
-		element: <ForgotPassword />,
+		path: "*",
+		element: <Root />,
 	},
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+	return (
+		<>
+			<RouterProvider router={router} />;
+		</>
+	);
 }
 
 export default App;
+
+function Root() {
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/offers" element={<Offers />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/forgot-password" element={<ForgotPassword />} />
+			</Routes>
+		</>
+	);
+}
