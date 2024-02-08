293fbb5d8b59693566e96953234a60cad7a6849a
Admayah <mayobami49@gmail.com>
2024-02-08 14:17:42 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+
+import "./App.css";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/sign-up",
+		element: <SignUp />,
+	},
+	{
+		path: "/sign-in",
+		element: <SignIn />,
+	},
+	{
+		path: "/offers",
+		element: <Offers />,
+	},
+	{
+		path: "/profile",
+		element: <Profile />,
+	},
+  {
+		path: "/",
+		element: <Profile />,
+	},
+  {
+		path: "/forgot-password",
+		element: <ForgotPassword />,
+	},
+]);
 
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+	return <RouterProvider router={router} />;
 }
 
 export default App;
