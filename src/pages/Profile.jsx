10de20b428ff072fdf10725089e647607f04b865
Admayah <mayobami49@gmail.com>
2024-02-09 13:46:26 +0100

    add edit functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { doc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
 	const auth = getAuth();
-	const navigate = useNavigate()
+	const navigate = useNavigate();
+	const [changeDetail, setChangeDetail] = useState();
 	const [formData, setFormData] = useState({
 		name: auth.currentUser.displayName,
 		email: auth.currentUser.email,
@@ -14,6 +18,30 @@ const Profile = () => {
 	const onLogout = () => {
 		auth.signOut();
 		navigate("/");
+	};
+
+	const onChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value });
+	};
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			if (auth.currentUser.displayName !== name) {
+				// update display name in firebase auth
+				await auth.currentUser.updateProfile({
+					displayName: name,
+				});
+
+				// update name in the firestore
+				const docRef = doc(db, "users", auth.currentUser.uid);
+				await updateDoc(docRef, {
+					name,
+				});
+			}
+			toast.success("Profile details updated")
+		} catch (error) {
+			toast.error("Could not update profile details");
+		}
 	};
 
 	return (
@@ -27,8 +55,11 @@ const Profile = () => {
 							name="name"
 							id="name"
 							value={name}
-							disabled
-							className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+							disabled={!changeDetail}
+							onChange={onChange}
+							className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+								changeDetail && "bg-red-200 focus:bg-red-200"
+							}`}
 						/>
 						<input
 							type="email"
@@ -41,8 +72,14 @@ const Profile = () => {
 						<div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
 							<p className="flex items-center">
 								Do you want to change your name?{" "}
-								<span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
-									Edit
+								<span
+									onClick={() => {
+										changeDetail && onSubmit();
+										setChangeDetail((prevState) => !prevState);
+									}}
+									className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+								>
+									{changeDetail ? "Apply change" : "Edit"}
 								</span>
 							</p>
 							<p
